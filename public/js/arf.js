bbeab540d754544627c3d41baa5581c41d7ebce1
krishpranav <krisna.pranav@gmail.com>
2021-05-15 16:49:21 +0530

    neated the web ap

--- public/js/arf.js
+++ public/js/arf.js
@@ -4,3 +4,38 @@ var margin = [20, 120, 20, 140],
     i = 0,
     duration = 1250,
     root;
+
+var tree = d3.layout.tree()
+    .size([height, width]);
+
+var diagonal = d3.svg.diagonal()
+    .projection(function(d) { return [d.y, d.x]; });
+
+var vis = d3.select("#body").append("svg:svg")
+    .attr("width", width + margin[1] + margin[3])
+    .attr("height", height + margin[0] + margin[2])
+  .append("svg:g")
+    .attr("transform", "translate(" + margin[3] + "," + margin[0] + ")");
+
+d3.json("arf.json", function(json) {
+  root = json;
+  root.x0 = height / 2;
+  root.y0 = 0;
+
+  function collapse(d) {
+    if (d.children) {
+      d._children = d.children;
+      d._children.forEach(collapse);
+      d.children = null;
+    }
+  }
+
+/*  function toggleAll(d) {
+    if (d.children) {
+      d.children.forEach(toggleAll);
+      toggle(d);
+    }
+  } */
+  root.children.forEach(collapse);
+  update(root);
+});
