ce141b407dc339557a5a8f86988f829134b8b535
krishpranav <krisna.pranav@gmail.com>
2021-05-15 16:51:55 +0530

    toogle

--- public/js/arf.js
+++ public/js/arf.js
@@ -138,3 +138,13 @@ function update(source) {
     d.y0 = d.y;
   });
 }
+
+function toogle(d) {
+    if (d.children) {
+        d._children = d.children;
+        d.children = null;
+    } else {
+        d.children = d._children
+        d._children = null;
+    }
+}
